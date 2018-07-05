d63bc0e45074d0a9115449ebb20b715f284fc62f
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-05 11:16:54 +0000

    automatically backed up by learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,2 +1,14 @@
 class SuperHero
+  attr_accessor :name, :power, :biography
+  @@all = []
+
+  def initialize(hash)
+    hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
